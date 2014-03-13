e574cdfab2b700a467ce54a27b5b74dacd138620
jantsankhorloo <jantsankhorloo@gmail.com>
2014-03-13 15:21:49 +0800

    Update calc.py
    It is an algorithm for a calculator capable in total 8 operations:
    
    1. Addition '+'
    2. Subtraction '-'
    3. Multiplication '*'
    4. Division '/'
    5. Taking powers '**'
    6. Showing a percent of c of a '%'
    7. Taking a root of any given number. In other words, taking 1/c powers 'root'
    8. Find the greatest common divisor of two numbers 'gcd'

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 def gcd(a, c):
+    """Returns the greatest common divisor of a and c by using Euclidean algorith"""
     while (c != 0):
         r = a%c
         a = c
@@ -6,6 +7,7 @@ def gcd(a, c):
     return a
 
 def calc(a, b, c):
+    """a and c are numbers you want to do operations assigned to b"""
     while True:
         try:
             if b == '+':
@@ -19,9 +21,9 @@ def calc(a, b, c):
             elif b == '**':
                 return a**float(c)
             elif b == '%':
-                return (float(c)/a)*100
+                return (float(c)/a)*100 #the sign % is used to shot what percent of a is c
             elif b == 'root':
-                return a**(1.0/c) 
+                return a**(1.0/c) #takes a root to any given variable to input 'c', kind of reverse power
             elif b == 'gcd':
                 return gcd(a, c)                                      
             else:
